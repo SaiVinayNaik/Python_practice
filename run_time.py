5ff0371f17432ed86a0d82c4585d63410e1f4205
SaiVinayNaik <151800640+SaiVinayNaik@users.noreply.github.com>
2026-09-11 07:47:46 +0530

    Add files via upload

---
+++ run_time.py
@@ -0,0 +1,83 @@
+#run_time input()
+
+'''a=int(input("enter the a value: "))
+b=int(input("enter the b value: "))
+print(a+b)'''
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+c=a+b
+print(c)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+b)'''
+
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+'''fname=input("Enter your first name: ")
+lname=input("Enter your last name: ")
+print((fname+" "+lname).title())'''
+
+'''a=complex(input("enter the a value"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+
+'''a=bool(input("enter the a value"))
+b=bool(input("enter the b value"))
+c=bool(input("enter the c value"))
+print(a+b+c)'''
+
+'''a="programming language"
+a.capitalize()
+print(a)'''
+
+'''a="programming"
+a.capitalize()
+print(a)'''
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''#student_profile
+Id_Number = int(input("Enter the Id Number: "))
+Name = input("Enter the name: ")
+Mobile_Number = int(input("Enter the Mobile Number: "))
+Mail_ID = input("Enter the Mail ID: ")
+College = input("Enter the College: ")
+Branch = input("Enter the Branch: ")
+print(".............Student Profile............")
+print("Id Number is: ",Id_Number)
+print("Name is: ",Name)
+print("Mobile Number is: ",Mobile_Number)
+print("Mail ID is: ",Mail_ID)
+print("College is: ",College)
+print("Branch is: ",Branch)'''
+
+'''a=[10,20,30,40,50]
+a.insert(2,25)
+print(a)'''
+
+'''a=["python","java","c"]
+a.extend("code")
+print(a)'''
+
+'''a=("apple","banana","mango")
+b=list(a)
+b.append("kiwi")
+c=tuple(b)
+print(c)'''
+
+a={"black","white","red"}
+a.remove("white")
+print(a)
